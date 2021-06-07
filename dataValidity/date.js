d86fd19cc8c34f76e5872038a5a99d32e39e37b0
soskgo <soskgo@naver.com>
2021-06-07 23:09:42 +0900

    날짜 유효성 검사 로직 추가

--- dataValidity/date.js
+++ dataValidity/date.js
@@ -1,21 +1,34 @@
-// 일자 유효성 검사 (0000-00-00,숫자)
+// 일자 유효성 검사 (0000-00-00, 숫자, 미래날짜x)
 
-const date = '2020-06-02' // 날짜 정상입력
-// const date = '' // 날짜 미입력 
-// const date = 'test-te-st' // 문자입력
-// const date = '#@#$^%$^' //특수문자
+const inputDate = '2021-06-02' // 입력날짜
 const datePattern = /^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$/
 
-try {
-    if (date) {
-        if (datePattern.test(date)) {
-            return console.log('날짜가 형식이 맞습니다. : ', date)
-        } else {
-            return console.log('날자 형식이 아닙니다.')
-        }
-    } else if (!date) {
-        return console.log('입력 날짜가 없습니다.')
+if (dateCheck(inputDate)) return console.log('입력 날짜가 형식에 맞습니다.')
+else if (!dateCheck(inputDate)) return console.log('입력 날짜 형식, 미래의 날짜 입력을 확인하세요.')
+
+function dateCheck(inputDate) {
+    try {
+        // 입력 날짜가 정규표현식에 맞는지 체크
+        if (datePattern.test(inputDate)) {
+            var now = nowDate()
+            if (inputDate > now) return false
+            else return true
+        } else return false
+    } catch (error) {
+        console.log(error)
     }
-} catch (error) {
-    console.log(error)
+}
+
+// 현제 날짜 구하기
+function nowDate() {
+    let today = new Date()
+    let year = today.getFullYear().toString() // 현재년도
+    let month = (today.getMonth() + 1).toString() // 현재월
+    let date = (today.getDate()).toString() // 현재날짜
+
+    if (month.length < 10 || date < 10) {
+        month = '0' + month
+        date = '0' + date
+    }
+    return year + '-' + month + '-' + date
 }
